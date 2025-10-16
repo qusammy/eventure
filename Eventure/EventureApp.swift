91281009160515e78e0729b8adba1c07eb3ef94f
qusammy <quinn.mbv@gmail.com>
2025-10-16 09:58:56 -0500

    Initial Commit

---
+++ Eventure/EventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventureApp.swift
+//  Eventure
+//
+//  Created by Maddy Quinn on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct EventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventureTests/EventureTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventureTests.swift
+//  EventureTests
+//
+//  Created by Maddy Quinn on 10/16/25.
+//
+
+import Testing
+@testable import Eventure
+
+struct EventureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
