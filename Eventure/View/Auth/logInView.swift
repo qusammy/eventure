53082a1da26ad0ab8500a67f7c0a70065e7a0f39
qusammy <quinn.mbv@gmail.com>
2025-10-25 18:27:28 -0500

    new signUpView

---
+++ Eventure/View/Auth/logInView.swift
@@ -0,0 +1,191 @@
+//
+//  logInView.swift
+//  Eventure
+//
+//  Created by Maddy Quinn on 10/16/25.
+//
+
+import SwiftUI
+import CoreMotion
+
+// Motion class
+
+class MotionManager: ObservableObject {
+    private var motion = CMMotionManager()
+    @Published var pitch: Double = 0.0
+    @Published var roll: Double = 0.0
+    
+    var createdNewAccount: Bool = false
+
+    init() {
+        motion.deviceMotionUpdateInterval = 1.0 / 60.0  // 60 updates per second
+        motion.startDeviceMotionUpdates(to: .main) { [weak self] data, error in
+            guard let data = data else { return }
+            self?.pitch = data.attitude.pitch
+            self?.roll = data.attitude.roll
+        }
+    }
+
+    deinit {
+        motion.stopDeviceMotionUpdates()
+    }
+}
+
+struct logInView: View{
+    
+    // Log-in variables and vm
+    
+    @StateObject private var viewModel = AuthViewModel()
+
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var username: String = ""
+    
+    @State private var showSignUpScreen = false
+
+    // Motion variable
+    
+    @StateObject private var motion = MotionManager()
+
+    var body: some View{
+        NavigationView{
+            if viewModel.user != nil {
+                ZStack{
+                    Image("kayakPicture")
+                        .resizable()
+                        .renderingMode(.original)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width:1000, height:1000)
+                        .ignoresSafeArea() .offset(x: motion.roll * 50, y: motion.pitch * 50)
+                        .animation(.easeOut(duration: 0.1), value: motion.roll)
+                        .blur(radius: 4)
+                    Rectangle()
+                        .ignoresSafeArea()
+                        .foregroundStyle(.white)
+                        .opacity(0.40)
+                    VStack{
+                        Image("eventureLogo")
+                            .resizable()
+                            .frame(width:300, height: 60)
+                            .padding(.bottom, 50)
+                        Button {
+                            viewModel.signOut()
+                        } label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(width:225, height:50)
+                                    .foregroundStyle(Color("darkColor"))
+                                Text("LOG OUT")
+                                    .foregroundStyle(Color.white)
+                            }
+                        }
+                        
+                    }
+                }
+            } else {
+                ZStack{
+                    Image("kayakPicture")
+                        .resizable()
+                        .renderingMode(.original)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width:1000, height:1000)
+                        .ignoresSafeArea() .offset(x: motion.roll * 50, y: motion.pitch * 50)
+                        .animation(.easeOut(duration: 0.1), value: motion.roll)
+                        .blur(radius: 4)
+                    Rectangle()
+                        .ignoresSafeArea()
+                        .foregroundStyle(.white)
+                        .opacity(0.40)
+                    VStack{
+                        Image("eventureLogo")
+                            .resizable()
+                            .frame(width:300, height: 60)
+                            .padding(.bottom, 50)
+                        
+                        // Email text field
+                        
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width:225, height:50)
+                            .foregroundStyle(Color("textFieldColor"))
+                            .overlay{
+                                TextField("", text: $email, prompt: Text("Email").foregroundColor(.white))
+                                    .disableAutocorrection(true)
+                                    .textInputAutocapitalization(.never)
+                                    .foregroundStyle(Color.white)
+                                    .padding(.leading, 5)
+                            }
+                        
+                        // Password secure field
+                        
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width:225, height:50)
+                            .foregroundStyle(Color("textFieldColor"))
+                            .overlay{
+                                SecureField("", text: $password, prompt: Text("Password").foregroundColor(.white))
+                                    .disableAutocorrection(true)
+                                    .textInputAutocapitalization(.never)
+                                    .foregroundStyle(Color.white)
+                                    .padding(.leading, 5)
+                            }
+                        
+                        // Forgot password button
+                        
+                        Button {
+                            viewModel.resetPassword(email: email)
+                        } label: {
+                            Text("Forgot Password")
+                                .foregroundStyle(Color("darkColor"))
+                            
+                        }.padding(.leading, 85)
+                        
+                        
+                        // Log in button
+                        
+                        Button {
+                            Task {
+                                await viewModel.signIn(email: email, password: password)
+                            }
+                        } label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(width:225, height:50)
+                                    .foregroundStyle(Color("darkColor"))
+                                Text("LOG IN")
+                                    .foregroundStyle(Color.white)
+                            }
+                        }.padding(.top, 15)
+                        
+                        Text("OR")
+                            .foregroundStyle(Color("darkColor"))
+                            .padding(10)
+                        
+                        // Create new account button
+                        
+                        Button {
+                            showSignUpScreen.toggle()
+                        } label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(width:225, height:50)
+                                    .foregroundStyle(Color("darkColor"))
+                                Text("CREATE ACCOUNT")
+                                    .foregroundStyle(Color.white)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $showSignUpScreen, onDismiss: {
+                    print("dismissed")
+                }) {
+                    signUpView()
+                }
+    
+    }
+}
+
+
+#Preview {
+    logInView()
+}
