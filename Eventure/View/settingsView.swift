d78024eb254dd280746852622602920b327a9a86
qusammy <quinn.mbv@gmail.com>
2025-11-11 11:17:17 -0600

    Sign up and log in errors handled

--- Eventure/View/settingsView.swift
+++ Eventure/View/settingsView.swift
@@ -1,15 +1,70 @@
 import SwiftUI
 
 struct settingsView: View {
+    
+    @StateObject private var viewModel = AuthViewModel()
+
     var body: some View {
-        LiquidChromeView(
-            baseColor: [0.28, 0.63, 0.63],
-            speed: 0.4,
-            amplitude: 0.25,
-            freqX: 4,
-            freqY: 3
-        )
+        ZStack{
+            LiquidChromeView(
+                baseColor: [0.28, 0.63, 0.63],
+                speed: 0.4,
+                amplitude: 0.25,
+                freqX: 4,
+                freqY: 3
+            )
+            .ignoresSafeArea()
+            
+            RoundedRectangle(cornerRadius: 25)
+                .padding(10)
+                .foregroundStyle(Color.white)
+                .opacity(0.80)
+            VStack{
+                Image("eventureLogo")
+                    .resizable()
+                    .frame(width:300, height: 60)
+                
+                List{
         
-        .ignoresSafeArea()
+                    Text("About")
+                        .foregroundStyle(Color("darkColor"))
+                        .font(Font.custom("UbuntuSans-Regular", size: 18))
+                        .fontWeight(.medium)
+                        .listRowBackground(Color.clear)
+
+                    Text("Privacy Policy")
+                        .foregroundStyle(Color("darkColor"))
+                        .font(Font.custom("UbuntuSans-Regular", size: 18))
+                        .fontWeight(.medium)
+                        .listRowBackground(Color.clear)
+                    
+                    Text("Take Survey")
+                        .foregroundStyle(Color("darkColor"))
+                        .font(Font.custom("UbuntuSans-Regular", size: 18))
+                        .fontWeight(.medium)
+                        .listRowBackground(Color.clear)
+
+                    Text("Log out")
+                        .foregroundStyle(Color("darkColor"))
+                        .font(Font.custom("UbuntuSans-Regular", size: 18))
+                        .fontWeight(.medium)
+                        .listRowBackground(Color.clear)
+                        .buttonStyle(BorderlessButtonStyle())
+                        .onTapGesture {
+                            viewModel.signOut()
+                        }
+
+                   
+
+                }
+                .scrollContentBackground(.hidden)
+                .frame(maxHeight:500)
+                
+            }
+        }
     }
+}
+
+#Preview {
+    settingsView()
 }
